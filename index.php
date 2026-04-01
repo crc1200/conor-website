e9b26da62cdc0f19d74019b38816f196b08e4b00
Conor Cunningham <conorcunningham1200@gmail.com>
2026-03-31 23:35:04 -0700

    updated php file

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php // The start of something great ?>
-<h1>Welcome to Conor's digital garden</h1>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <?php wp_head(); ?> 
+</head>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+    <h1>Conor's Digital Garden is under construction</h1>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
